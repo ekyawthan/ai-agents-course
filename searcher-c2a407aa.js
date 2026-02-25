686357f2517b55bb0fe2a6f693cb3ec32dcd7e2a
ekyawthan <ekyawthan@users.noreply.github.com>
2026-02-25 00:43:31 +0000

    deploy: 9289781422713065ef56ad3884d70e777f595500

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5a130dfb.js',
+                path_to_root + 'searchindex-07650ce6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
